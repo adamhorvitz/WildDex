8d0682d2e3c5c5c23cb28be1e71f664f984a717b
Lexline Johnson <lexlinejj27@gmail.com>
2024-11-24 17:16:50 -0500

    Initial Commit

---
+++ WildDex/WildDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildDexApp.swift
+//  WildDex
+//
+//  Created by Lexline Johnson on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WildDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
