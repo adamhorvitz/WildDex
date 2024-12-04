819f1ef50ed97acb5b477a0fa3990fd70a0bbe30
mklinzing <marieklinzing@gmail.com>
2024-12-04 11:13:01 -0500

    updated quicksort to include ids. Now prints the same as heapsort.

--- WildDex/redListScraper.cpp
+++ WildDex/redListScraper.cpp
@@ -82,28 +82,29 @@ std::string makeRequest(const std::string& url) {
 //_______________________________________
 
 //reference: https://www.geeksforgeeks.org/quick-sort-algorithm/
-int pivotLogic(vector<string>& nameArray, int low, int high) {
+int pivotLogic(vector<string>& names, vector<int>& ids, int low, int high) {
     //preset pivot point
-    string pivot = nameArray[high];
+    string pivot = names[high];
     int index = low - 1;
 
     for (int i = low; i <= high - 1; i++) {
-        if(nameArray[i] < pivot) {
+        if(names[i] < pivot) {
             index++;
-            swap(nameArray[index], nameArray[i]);
+            swap(names[index], names[i]);
+            swap(ids[index], ids[i]);
         }
     }
-    swap(nameArray[index + 1], nameArray[high]);
+    swap(names[index + 1], names[high]);
+    swap(ids[index + 1], ids[high]);
     return index + 1;
 }
-
-void quickSort(vector<string>& nameArray, int low, int high) {
+void quickSort(vector<string>& names, vector<int>& ids, int low, int high) {
     if (low < high) {
-        int separation = pivotLogic(nameArray, low, high);
+        int separation = pivotLogic(names, ids, low, high);
 
         //recursion for element calls
-        quickSort(nameArray, low, separation - 1);
-        quickSort(nameArray, separation + 1, high);
+        quickSort(names, ids, low, separation - 1);
+        quickSort(names, ids, separation + 1, high);
     }
 }
 //_____________________________
@@ -135,27 +136,27 @@ int fetchRedListData(const std::string& apiToken) {
     }
 
     vector<string> speciesNames;
+    vector<int> speciesIDs;
 
     //collect species names
     for (const auto& species : parsedData["result"]) {
         std::string scientificName = species.value("scientific_name", "Unknown");
         std::string category = species.value("category", "");
+        int taxonID = species.value("taxonid", 0);
 
         // critically endangered species only
         if (category == "CR") {
-            // fetch the common name using a separate API request
-//            std::string commonName = fetchCommonName(scientificName, apiToken);
             speciesNames.push_back(scientificName);
+            speciesIDs.push_back(taxonID);
         }
     }
     //sort species
-    quickSort(speciesNames, 0, speciesNames.size() - 1);
+    quickSort(speciesNames, speciesIDs, 0, speciesNames.size() - 1);
 
     // display species in vector as descending order of count
     std::cout << "\nCritically Endangered Species in the United States ordered by descending count:\n";
-    for (const auto& name : speciesNames) {
-        //std::cout << name << " with ID: " << name.count << std::endl;
-        std::cout << name << std::endl;
+    for (size_t i = 0; i < speciesNames.size(); i++) {
+        std::cout << speciesNames[i] << " with ID: " << speciesIDs[i] << std::endl;
     }
 
     return 0;
